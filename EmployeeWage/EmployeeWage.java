1be3050e7921d426ac8384e0179b234496811555
shashank srivastava <shashank.1696@gmail.com>
2020-07-17 00:01:08 +0530

    Computing Part Time Employee Wage

--- EmployeeWage/EmployeeWage.java
+++ EmployeeWage/EmployeeWage.java
@@ -3,21 +3,25 @@ public class EmpWage {
 	public static void main(String[] args) {
 	
 		int isFullTime=1;
+		int isPartTime=2
 		int empRatePerHrs=20;
 
 		int empWorkHrs=0;
 		int empWage=0;
 
-		double empAttendance = Math.floor(Math.random() *10)%2;
+		double empAttendance = Math.floor(Math.random() *10)%3;
 	
 		if(empAttendance == isFullTime)
 		{
 			empWorkHrs=8;
 		}
+		else if(empAttendance == isPartTime){
+			empWorkHrs=4;
+		}
 		else
-			empWorkHrs=0;
+			empHrs = 0;
 			empWage=empWorkHrs * empRatePerHrs;
-			System.out.println("Emp Wage: "+empWage);  
+			System.out.println("Employee Wage: "+empWage);
 		}
 
 }
