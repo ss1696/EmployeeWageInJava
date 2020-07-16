d4dceabde64c52145e0a0cef0ad150b78de02bfe
shashank srivastava <shashank.1696@gmail.com>
2020-07-17 00:12:22 +0530

    Computing Employee Wage Using SwitchCase

--- EmployeeWage/EmployeeWage.java
+++ EmployeeWage/EmployeeWage.java
@@ -1,25 +1,28 @@
 public class EmpWage {
 
+	public static final int isFullTime = 1;
+	public static final int isPartTime = 2;
+	public static final int empRatePerHrs = 20;
+	
 	public static void main(String[] args) {
 	
-		int isFullTime=1;
-		int isPartTime=2
-		int empRatePerHrs=20;
-
 		int empWorkHrs=0;
 		int empWage=0;
 
-		double empAttendance = Math.floor(Math.random() *10)%3;
+		int empAttendance = (int)Math.floor(Math.random() *10)%3;
 	
-		if(empAttendance == isFullTime)
-		{
-			empWorkHrs=8;
-		}
-		else if(empAttendance == isPartTime){
-			empWorkHrs=4;
-		}
-		else
-			empHrs = 0;
+		switch(empAttendance){
+		
+		case isFullTime: 
+				empWorkHrs=8;
+				break;
+		case isPartTimr:
+				empWorkHrs=4;
+				break;
+		default:
+				empWorkHrs=0;
+		}	
+		
 			empWage=empWorkHrs * empRatePerHrs;
 			System.out.println("Employee Wage: "+empWage);
 		}
