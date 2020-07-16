0c304fdf340b6a95ace2b79270625b4fe8f4f7f2
shashank srivastava <shashank.1696@gmail.com>
2020-07-16 23:14:19 +0530

    Checking Attendance of Employee

---
+++ EmployeeWage/EmployeeWage.java
@@ -0,0 +1,17 @@
+public class EmpWage {
+
+	public static void main(String[] args) {
+	
+		int isFullTime=1;
+		double empAttendance = Math.floor(Math.random() *10)%2;
+	
+		if(empAttendance == isFullTime)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+			System.out.println("Employee is Abscent");
+
+	}
+
+}
