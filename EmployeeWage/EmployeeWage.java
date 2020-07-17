1cf042825c6704ccaa93d7c425d1c445c68ce66b
shashank srivastava <shashank.1696@gmail.com>
2020-07-18 00:35:18 +0530

    Refactor: Employee Wage For multiple Companies

--- EmployeeWage/EmployeeWage.java
+++ EmployeeWage/EmployeeWage.java
@@ -1,18 +1,20 @@
-public class EmpWage {
+public class EmployeeWage {
 
 	public static final int isFullTime = 1;
 	public static final int isPartTime = 2;
 	public static final int empRatePerHrs = 20;
 	public static final int totalWorkDays = 20;
 	public static final int maxWorkHrs = 100;
+	public static String company="";
 
-	public static int computeEmpWage() {
+	public static int computeEmpWage(String company) {
 	
 		int empWorkHrs=0;
 		int totalWorkingHrs=0;
 		int totalWorkingDays=0;
 		int totalEmpWage=0;
 
+		EmployeeWage.company=company;
 		while(totalWorkingDays<totalWorkDays &&  totalWorkingHrs<=maxWorkHrs) {
 			
 			int empAttendance = (int)Math.floor(Math.random() *10)%3;
@@ -22,7 +24,7 @@ public class EmpWage {
                         case isFullTime:
                                         empWorkHrs=8;
                                         break;
-                        case isPartTimr:
+                        case isPartTime:
                                         empWorkHrs=4;
                                         break;
                         default:
@@ -38,16 +40,17 @@ public class EmpWage {
 			}
 		}
 		
-		totalEmpWape=totalWorkingHrs*empRatePerHrs;
-
-		return totalEmpWage;
-
+		return totalWorkingHrs*empRatePerHrs;
 	}
 
 	public static void main(String[] args) {
 
-		int totalEmpWage=computeEmpWage();
-		System.out.println("Total Emp Wage : "+totalEmpWage);
+		int totalEmpWage=computeEmpWage("Sony");
+		System.out.println("Total Emp Wage in "+EmployeeWage.company+" Company ="+totalEmpWage);
+
+		int totalEmpWage=computeEmpWage("Samsung");
+                System.out.println("Total Emp Wage in "+EmployeeWage.company+" Company $
+
 	}
 
 }
